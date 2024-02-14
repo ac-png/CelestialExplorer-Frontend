74c84551c72f120d725788e29b4ac03f2bf64b8d
Alice Corry <alicecorry@icloud.com>
2024-02-14 19:58:21 +0000

    Getting Astronomy Photo of the Day and Known Count information from Public APIs using Axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,58 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function Home() {
+    const [objects, setObjects] = useState([]);
+    const [photo, setPhoto] = useState(null); 
+    
+    useEffect(() => {
+        axios
+            .get(`https://api.le-systeme-solaire.net/rest/knowncount`)
+            .then((response) => {
+                console.log(response.data.knowncount);
+                setObjects(response.data.knowncount);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    useEffect(() => {
+        axios
+            .get(`https://api.nasa.gov/planetary/apod?api_key=niz11pa6Xj1H0HrJijvK3jGR72Lf9XGmXsa9bdsS`)
+            .then((response) => {
+                console.log(response.data);
+                setPhoto(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const objectList = objects.map(object => {
+        return (
+            <div key={object.id}>
+                <p>{object.id}</p>
+                <p>{object.knownCount}</p>
+                <hr />
+            </div>
+        );
+    });
+
     return (
-        <h1>This is the Homepage!</h1>
+        <>
+            {photo && (
+                <img
+                    src={photo.url}
+                    alt={photo.title}
+                    style={{ width: '200px', height: 'auto' }}
+                />
+            )}
+            <h2>Known Count</h2>
+            { objectList }
+        </>
     );
+    
 }
 
 export default Home;
