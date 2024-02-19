57d1eb0d7b06037f5b972ad9b8d31e64e16ca0d0
Alice Corry <alicecorry@icloud.com>
2024-02-19 11:35:07 +0000

    Fixing some formatting

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,34 +2,34 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Home() {
-const [counts, setCounts] = useState([]);
+    const [counts, setCounts] = useState([]);
 
-useEffect(() => {
-    axios
-    .get('https://api.le-systeme-solaire.net/rest/knowncount')
-    .then((response) => {
-        // console.log(response.data.knowncount);
-        setCounts(response.data.knowncount);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-}, []);
+    useEffect(() => {
+        axios
+        .get('https://api.le-systeme-solaire.net/rest/knowncount')
+        .then((response) => {
+            // console.log(response.data.knowncount);
+            setCounts(response.data.knowncount);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }, []);
 
-const countList = counts.map((count) => (
-    <div key={count.id}>
-    <p>{count.id}</p>
-    <p>{count.knownCount}</p>
-    <hr />
-    </div>
-));
+    const countList = counts.map((count) => (
+        <div key={count.id}>
+            <p>{count.id}</p>
+            <p>{count.knownCount}</p>
+            <hr />
+        </div>
+    ));
 
-return (
-    <>
-        <h2>Known Count</h2>
-        {countList}
-    </>
-);
+    return (
+        <>
+            <h2>Known Count</h2>
+            {countList}
+        </>
+    );
 }
 
 export default Home;
