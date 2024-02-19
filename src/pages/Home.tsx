406475ab803689900d6987a998d6213b8b38d01a
Alice Corry <alicecorry@icloud.com>
2024-02-19 11:27:47 +0000

    Removing Photo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Home() {
@@ -17,18 +17,6 @@ useEffect(() => {
     });
 }, []);
 
-useEffect(() => {
-    axios
-    .get('https://api.nasa.gov/planetary/apod?api_key=niz11pa6Xj1H0HrJijvK3jGR72Lf9XGmXsa9bdsS')
-    .then((response) => {
-        // console.log(response.data);
-        setPhoto(response.data);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-}, []);
-
 const countList = counts.map((count) => (
     <div key={count.id}>
     <p>{count.id}</p>
@@ -39,26 +27,6 @@ const countList = counts.map((count) => (
 
 return (
     <>
-        {photo && (
-            <div>
-            {photo.media_type === 'image' ? (
-                <img
-                src={photo.url}
-                alt={photo.title}
-                style={{ width: '200px', height: 'auto' }}
-                />
-            ) : (
-                <iframe
-                width="560"
-                height="315"
-                src={photo.url}
-                title="YouTube video player"
-                frameBorder="0"
-                allowFullScreen
-                ></iframe>
-            )}
-            </div>
-        )}
         <h2>Known Count</h2>
         {countList}
     </>
