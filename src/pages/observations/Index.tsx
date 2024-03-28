7ea03f74b158548f02fa08e7d7d71a8287ce89e9
Alice Corry <alicecorry@icloud.com>
2024-03-28 22:45:25 +0000

    Adding the celestial_body_id to englishName from the bodies api

--- src/pages/observations/Index.tsx
+++ src/pages/observations/Index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 import { fetchObservations, deleteByUUID } from '../../services/APIService/observations';
-import { fetchBodyById } from '../../services/APIService/bodies';
+import { fetchBodyById } from "../../services/APIService/bodies";
 import { formatDate, formatTime } from '../../utilities/format';
 
 function Index() {
@@ -15,7 +15,23 @@ function Index() {
             if (response && response.message === "No observations found!") {
                 setObservations([]);
             } else {
-                setObservations(response);
+                const bodyPromises = response.map(observation => {
+                    return fetchBodyById(observation.celestial_body_id);
+                });
+                Promise.all(bodyPromises)
+                .then(bodyResponses => {
+                    const observationsWithBodies = response.map((observation, index) => {
+                        return {
+                            ...observation,
+                            bodyName: bodyResponses[index].englishName
+                        };
+                    });
+                    setObservations(observationsWithBodies);
+                })
+                .catch(error => {
+                    console.error('Error fetching body information:', error);
+                    setObservations(response);
+                });
             }
             setIsLoading(false);
         })
@@ -23,7 +39,8 @@ function Index() {
             console.error('Error fetching observations:', error);
             setIsLoading(false);
         });
-    }, []);
+        
+    }, []);  
 
     const handleDelete = async (uuid) => {
         let token = localStorage.getItem('token');
@@ -40,38 +57,29 @@ function Index() {
             console.error('Error deleting observation:', error);
         }
     };
-    
-    const fetchBodyDetails = async (id) => {
-        try {
-            const bodyDetails = await fetchBodyById(id);
-            console.log('Body details:', bodyDetails);
-            return bodyDetails.name;
-        } catch (error) {
-            console.error('Error fetching body details:', error);
-            return '';
-        }
-    };
 
     return (
         <div className="py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <Link className="mb-4 px-4 py-2 border border-green-200 text-green-700 rounded-md btn-link btn-lg mb-2" to='/dashboard/observations/create'>+ New Observation</Link>
+                <div className="mb-8 flex justify-between items-center">
+                    <h1 className="text-3xl font-bold">Observations Dashboard</h1>
+                    <Link to='/dashboard/observations/create' className="mb-4 px-4 py-2 border border-green-200 text-green-700 rounded-md btn-link btn-lg mb-2">New Observation</Link>
+                </div>
                 {isLoading ? (
                     <p>Loading...</p>
                 ) : observations.length === 0 ? (
                     <p>No Observations Found</p>
                 ) : (
                     observations.map((observation) => (
-                        <div className=" card my-6 p-6">
+                        <div className="card my-6 p-6">
                             <h2 className="font-bold text-2xl">
-                                {observation.celestial_body_id}
+                                {observation.bodyName}
                             </h2>
                             <p className="mt-2">
                                 {observation.description}
                             </p>
                             <span className="block mt-4 text-sm opacity-70">{formatDate(observation.date)} {formatTime(observation.time)}</span>
                             <button className="mt-4 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mr-3 focus-visible:outline-indigo-600" onClick={() => handleDelete(observation.uuid)}><i className="fa-solid fa-trash mr-2"></i>Delete</button>
-                            <button className="mt-4 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={() => fetchBodyDetails(observation.celestial_body_id)}>Fetch Body Details</button>
                         </div>
                     ))
                 )}
