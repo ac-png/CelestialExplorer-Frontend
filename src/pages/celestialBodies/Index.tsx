6d795cf54b88124390063fda03fbf1c0af6822b9
Alice Corry <alicecorry@icloud.com>
2024-04-03 12:39:53 +0100

    Adding search functionality

--- src/pages/celestialBodies/Index.tsx
+++ src/pages/celestialBodies/Index.tsx
@@ -6,6 +6,7 @@ function Index() {
     const [bodies, setBodies] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [selectedBodyType, setSelectedBodyType] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         fetchBodies()
@@ -47,6 +48,10 @@ function Index() {
         setSelectedBodyType(bodyType === selectedBodyType ? null : bodyType);
     }
 
+    function handleSearchChange(event) {
+        setSearchTerm(event.target.value);
+    }
+
     return (
         <>
             <div className="px-4">
@@ -57,7 +62,7 @@ function Index() {
                 ) : (
                     <>
                         <div>
-                            <ul className="mt-5 grid grid-cols-3 gap-4 mt-3">
+                            <ul className="mt-3 grid grid-cols-3 gap-4 mt-3">
                                 <li className="font-bold text-center p-3" onClick={() => handleBodyTypeClick(null)}>All</li>
                                 <li className="font-bold text-center p-3 dwarf-planet" onClick={() => handleBodyTypeClick('dwarf planet')}>Dwarf Planet</li>
                                 <li className="font-bold text-center p-3 planet" onClick={() => handleBodyTypeClick('planet')}>Planet</li>
@@ -66,6 +71,17 @@ function Index() {
                                 <li className="font-bold text-center p-3 comet" onClick={() => handleBodyTypeClick('comet')}>Comet</li>
                                 <li className="font-bold text-center p-3 asteroid" onClick={() => handleBodyTypeClick('asteroid')}>Asteroid</li>
                             </ul>
+                            <p>
+                                <strong>Search for Celestial Body:</strong>
+                                <input
+                                    type="text"
+                                    placeholder="Search..."
+                                    value={searchTerm}
+                                    onChange={handleSearchChange}
+                                    className="ml-3 mt-5 p-2 rounded-md border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    style={{ color: 'black' }}
+                                />
+                            </p>
                         </div>
                         <div>
                             <h2 className="mt-5 text-2xl font-bold leading-9 tracking-tight">
@@ -73,7 +89,10 @@ function Index() {
                             </h2>
                         </div>
                         <div className="grid grid-cols-3 gap-4 mt-3">
-                            {bodies.filter(body => !selectedBodyType || body.bodyType.toLowerCase() === selectedBodyType.toLowerCase()).map((body) => (
+                            {bodies.filter(body => 
+                                (!selectedBodyType || body.bodyType.toLowerCase() === selectedBodyType.toLowerCase()) &&
+                                (body.englishName.toLowerCase().includes(searchTerm.toLowerCase()))
+                            ).map((body) => (
                                 <Link to={`/celestial-bodies/${body.id}`} key={body.id}>
                                     <div key={body.id} className={`${getBodyTypeClassName(body.bodyType)} p-4 text-center`}>
                                         <h2 className="font-bold text-xl">{body.englishName}</h2>
