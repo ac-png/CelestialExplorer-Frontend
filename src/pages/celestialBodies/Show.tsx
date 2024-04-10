6b6194e5c7ece805532ae7d7ae5767df28e1d60b
Alice Corry <alicecorry@icloud.com>
2024-04-10 09:23:25 +0100

    Adding observations by body to the body show page

--- src/pages/celestialBodies/Show.tsx
+++ src/pages/celestialBodies/Show.tsx
@@ -1,14 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { fetchBodyById } from '../../services/APIService/bodies';
+import { fetchObservationByBody, deleteByUUID } from '../../services/APIService/observations';
 import { useEffect, useState } from "react";
+import { formatDate, formatTime } from '../../utilities/format';
+import { Rating } from '../../../node_modules/@smastrom/react-rating/dist/index';
 
 function Show() {
     let { id } = useParams();
     const [body, setBody] = useState(null);
+    const [observations, setObservations] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         setIsLoading(true);
+        let token = localStorage.getItem('token');
 
         fetchBodyById(id)
         .then((response) => {
@@ -23,7 +28,31 @@ function Show() {
             console.error('Error fetching bodies:', error);
             setIsLoading(false);
         });
+
+        fetchObservationByBody(token, id)
+        .then((data) => {
+            setObservations(data);
+        })
+        .catch((error) => {
+            console.error('Error fetching observations:', error);
+        });
     }, [id]);
+
+    const handleDelete = async (uuid) => {
+        let token = localStorage.getItem('token');
+        try {
+            await deleteByUUID(token, uuid);
+            fetchObservationByBody(token, id)
+            .then(response => {
+                setObservations(response);
+            })
+            .catch(error => {
+                console.error('Error fetching observations after deletion:', error);
+            });
+        } catch (error) {
+            console.error('Error deleting observation:', error);
+        }
+    };
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -37,8 +66,8 @@ function Show() {
         <div className="py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div className="overflow-hidden">
+                    <h1 className="text-center text-5xl font-bold mb-5">{body.englishName}</h1>
                     <div className="bg-gray-800 shadow-md rounded-md p-6">
-                        <h1 className="text-center text-xl font-bold mb-4">{body.englishName}</h1>
                         <div className="grid grid-cols-2">
                             <div style={{ display: 'flex', justifyContent: 'center' }}>
                                 <div>
@@ -85,6 +114,37 @@ function Show() {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <h1 className="text-2xl font-bold mt-5">Observations</h1>
+                    <div className="bg-gray-800 shadow-md rounded-md p-6 mt-5" style={{ display: 'inline-block' }}>
+                        {isLoading ? (
+                            <p>Loading...</p>
+                        ) : observations.length === 0 ? (
+                            <div>
+                                <div className="text-center">
+                                    <p className="text-red-600 text-2xl font-bold mb-4">No Observations Found!</p>
+                                    <p className="mb-8">Start by creating a new observation.</p>
+                                    <a href="/dashboard/observations/create" className="mb-4 px-4 py-2 border border-green-200 text-green-700 rounded-md btn-link btn-lg mb-2">Create New Observation</a>
+                                </div>
+                            </div>
+                        ) : (
+                            observations.map((observation) => (
+                                observations.map((observation) => (
+                                    <div className="mt-5" key={observation.id}>
+                                        <p className="mt-2">
+                                            <strong>Description: </strong>{observation.description}
+                                        </p>
+                                        <p className="flex items-center">
+                                            <strong>Rating: </strong><Rating className="block mt-2 opacity-70" style={{ maxWidth: 250 }} value={observation.rating} readOnly />
+                                        </p>
+                                        <span className="block mt-2 text-sm opacity-70">{formatDate(observation.date)} {formatTime(observation.time)}</span>
+                                        <Link to={`/dashboard/observations/edit/${observation.uuid}`} className="mt-4 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mr-3 focus-visible:outline-indigo-600"><i className="fa-solid fa-pencil mr-3"></i>Edit</Link>
+                                        <button className="mt-4 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mr-3 focus-visible:outline-indigo-600" onClick={() => handleDelete(observation.uuid)}><i className="fa-solid fa-trash mr-2"></i>Delete</button>
+                                    </div>
+                                ))
+                                
+                            ))
+                        )}
                     </div>
                 </div>
             </div>
