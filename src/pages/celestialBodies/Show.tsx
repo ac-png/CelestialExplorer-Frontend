ef54ba80299edddbf27f09760a88016df5cd48e1
Alice Corry <alicecorry@icloud.com>
2024-04-10 16:29:09 +0100

    Adding sample placeholder image and make moon list look nicer

--- src/pages/celestialBodies/Show.tsx
+++ src/pages/celestialBodies/Show.tsx
@@ -67,21 +67,17 @@ function Show() {
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div className="overflow-hidden">
                     <h1 className="text-center text-5xl font-bold mb-5">{body.englishName}</h1>
+                    <img
+                        src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
+                        className="rounded-t-lg"
+                        alt="..."
+                    />
                     <div className="bg-gray-800 shadow-md rounded-md p-6">
                         <div className="grid grid-cols-2">
                             <div style={{ display: 'flex', justifyContent: 'center' }}>
                                 <div>
                                     <p><span className="font-bold">Body Type:</span> {body.bodyType || "N/A"}</p>
                                     <p><span className="font-bold">Dimension:</span> {body.dimension || "N/A"}</p>
-                                    {body.moons && body.moons.length > 0 ? (
-                                        <p>
-                                            <span className="font-bold">Moons:</span> {body.moons.map((moon, index) => moon.moon).join('. ')}
-                                        </p>
-                                    ) : (
-                                        <p>
-                                            <span className="font-bold">Moons:</span> N/A
-                                        </p>
-                                    )}
                                     <p><span className="font-bold">Semi-Major Axis:</span> {body.semimajorAxis || "N/A"} km</p>
                                     <p><span className="font-bold">Perihelion:</span> {body.perihelion || "N/A"}</p>
                                     <p><span className="font-bold">Aphelion:</span> {body.aphelion || "N/A"}</p>
@@ -115,6 +111,20 @@ function Show() {
                             </div>
                         </div>
                     </div>
+                    <h1 className="text-2xl font-bold mt-5">Moons</h1>
+                    {body.moons && body.moons.length > 0 ? (
+                        <div className="bg-gray-800 shadow-md rounded-md p-6 mt-5 grid grid-cols-3 gap-4 mt-3">
+                            {body.moons.map((moon, index) => (
+                                <Link to={`/celestial-bodies/${moon.moon}`} key={index} className='p-4 text-center moon m-1'>
+                                    <h2 className="font-bold text-xl">{moon.moon}</h2>
+                                </Link>
+                            ))}
+                        </div>
+                    ) : (
+                        <p className="bg-gray-800 shadow-md rounded-md p-6 mt-5">
+                            <p className="text-red-600 text-2xl font-bold">This celestial body has no moons!</p>
+                        </p>
+                    )}
                     <h1 className="text-2xl font-bold mt-5">Observations</h1>
                     <div className="bg-gray-800 shadow-md rounded-md p-6 mt-5" style={{ display: 'inline-block' }}>
                         {isLoading ? (
@@ -124,22 +134,25 @@ function Show() {
                                 <div className="text-center">
                                     <p className="text-red-600 text-2xl font-bold mb-4">No Observations Found!</p>
                                     <p className="mb-8">Start by creating a new observation.</p>
-                                    <a href="/dashboard/observations/create" className="mb-4 px-4 py-2 border border-green-200 text-green-700 rounded-md btn-link btn-lg mb-2">Create New Observation</a>
+                                    <a href="/dashboard/observations/create" className="mb-4 px-4 py-2 border border-green-600 text-green-600 rounded-md btn-link btn-lg mb-2 hover:bg-green-600 hover:text-gray-800">Create New Observation</a>
                                 </div>
                             </div>
                         ) : (
                             observations.map((observation) => (
                                 observations.map((observation) => (
-                                    <div className="mt-5" key={observation.id}>
+                                    <div key={observation.id}>
                                         <p className="mt-2">
                                             <strong>Description: </strong>{observation.description}
                                         </p>
-                                        <p className="flex items-center">
-                                            <strong>Rating: </strong><Rating className="block mt-2 opacity-70" style={{ maxWidth: 250 }} value={observation.rating} readOnly />
+                                        <p className="mt-2">
+                                            <strong>Location: </strong>{observation.latitude}, {observation.longitude}
                                         </p>
-                                        <span className="block mt-2 text-sm opacity-70">{formatDate(observation.date)} {formatTime(observation.time)}</span>
-                                        <Link to={`/dashboard/observations/edit/${observation.uuid}`} className="mt-4 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mr-3 focus-visible:outline-indigo-600"><i className="fa-solid fa-pencil mr-3"></i>Edit</Link>
-                                        <button className="mt-4 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mr-3 focus-visible:outline-indigo-600" onClick={() => handleDelete(observation.uuid)}><i className="fa-solid fa-trash mr-2"></i>Delete</button>
+                                        <p className="block mt-2"><strong>Date and Time: </strong>{formatDate(observation.date)} {formatTime(observation.time)}</p>
+                                        <p className="flex items-center mb-3">
+                                            <strong>Rating: </strong><Rating className="block mt-2 opacity-100" style={{ maxWidth: 250 }} value={observation.rating} readOnly />
+                                        </p>
+                                        <Link to={`/dashboard/observations/edit/${observation.uuid}`} className="px-4 border border-yellow-600 text-yellow-600 mr-3 py-2.5 rounded-md btn-link btn-lg mb-2 hover:bg-yellow-600 hover:text-gray-800"><i className="fa-solid fa-pencil mr-3"></i>Edit</Link>
+                                        <button className="px-4 py-2 border border-red-600 text-red-600 mr-3 bg-gray-800 rounded-md btn-link btn-lg mb-2 hover:bg-red-600 hover:text-gray-800" onClick={() => handleDelete(observation.uuid)}><i className="fa-solid fa-trash mr-2"></i>Delete</button>
                                     </div>
                                 ))
                                 
