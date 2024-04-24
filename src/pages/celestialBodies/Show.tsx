9a2561d9b75937d74675f0ff32f0646c01703aee
Alice Corry <alicecorry@icloud.com>
2024-04-24 14:22:19 +0100

    adding ? if bodyType doesn't exist

--- src/pages/celestialBodies/Show.tsx
+++ src/pages/celestialBodies/Show.tsx
@@ -63,7 +63,7 @@ function Show() {
     }
 
     let imageUrl;
-    switch (body.bodyType.toLowerCase()) {
+    switch (body.bodyType?.toLowerCase()) {
         case 'dwarf planet':
             imageUrl = 'https://images.unsplash.com/photo-1614314107768-6018061b5b72?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZHdhcmYlMjBwbGFuZXR8ZW58MHx8MHx8fDA%3D';
             break;
